076e549e1f2fbc1d5124b7cc7037d4dbe3b26608
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2026-03-18 15:03:46 +0530

    chore: prepare frontend for Vercel deployment (env-based API URL, vercel.json SPA routing)

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api' // Valid due to Vite proxy
+  baseURL: import.meta.env.VITE_API_URL || '/api'
 });
 
 api.interceptors.request.use((config) => {
